d3a815f8bc96401d37c253654b54faa76860764a
ABEC <euroset2016sw43@gmail.com>
2023-12-11 03:07:11 +0300

    Added pause screen
    Without pause itself

--- Nodes/UI/ui.gd
+++ Nodes/UI/ui.gd
@@ -1,5 +1,22 @@
 extends Control
+signal toggle_pause(paused: bool)
 
 
 func change_score(new_score: int):
 	$InGameUI/Score.text = "Your score is: " + str(new_score)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		if $Pause.visible:
+			emit_signal("toggle_pause", false)
+			$Pause.visible = false
+			$InGameUI.visible = true
+		else:
+			emit_signal("toggle_pause", true)
+			$Pause.visible = true
+			$InGameUI.visible = false
+
+func _on_continue_button_up():
+	$Pause.visible = false
+	$InGameUI.visible = true
+	emit_signal("toggle_pause", false)
